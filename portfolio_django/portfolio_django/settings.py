d25685997410cd38283e5b2c7489099e36ac0977
Jon Rhymes <jonrhymes@Alisons-MacBook-Air.local>
2020-06-25 11:03:09 -0400

    Added requirements and gitignore

--- portfolio_django/portfolio_django/settings.py
+++ portfolio_django/portfolio_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c87d0fd_ww*-te@v74*#l03pg+!m$l_*!1d$vo&9dy@65nqxe7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['167.99.1.241', '127.0.0.1', 'localhost', 'jonathanrhymes.com']
 
 
 # Application definition
